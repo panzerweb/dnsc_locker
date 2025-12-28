a685053cf9b034c2f983c017e8ffae9ecf6a66a5
Panzerweb <selwynvillar@gmail.com>
2025-12-28 22:22:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dnsc_locker
+DNSC Locker Rental Management System Mobile Application
---
+++ android/app/src/main/kotlin/com/example/dnsc_locker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dnsc_locker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
